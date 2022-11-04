988a3d3c79693f9ff91c430afdeb31d0dce1a2d5
Gonzalo Raymond <you@example.com>
2022-11-04 20:50:29 -0300

    funcionalidad de busqueda

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,92 @@
-const mokapiUrl = `https://63651969046eddf1bae51995.mockapi.io/users`
-let users = {};
+const mokapiUsersUrl = `https://63651969046eddf1bae51995.mockapi.io/users/`
+
+const resultsList = document.getElementById("results");
+const searchInput = document.getElementById("inputGet1Id");
+const searchBtn = document.getElementById("btnGet1");
+
+searchInput.addEventListener("input", () => {
+
+    if(searchInput.value < 1){
+        searchInput.value = "";
+    }
+
+});
+
+searchBtn.addEventListener("click", async () => {
+
+    let users = {};
+
+    let searchInputValue = searchInput.value;
+
+    if(searchInputValue > 0){
+
+        let mokapiUserUrl = `${mokapiUsersUrl}${searchInputValue}`;
+
+        const resultObj = await getJSONData(mokapiUserUrl)
+        if (resultObj.status === "ok"){
+            users = resultObj;
+            
+            showUsersList(users);
+        }else{
+            showUsersList("clearList");
+            showErrorAlert();
+        }
+
+    }else{
+        const resultObj = await getJSONData(mokapiUsersUrl)
+        if (resultObj.status === "ok"){
+            users = resultObj;
+            showUsersList(users);  
+        }else{
+            showErrorAlert();
+        }
+    }
+    
+});
+
+const showErrorAlert = () => {
+    document.getElementById("alert-error").classList.add("show");
+    setTimeout(()=>{
+        document.getElementById("alert-error").classList.remove("show");  
+    }, 2500);
+}
+
+const showUsersList = (arrayObj) =>{
+
+    let usersContent = "";
+    
+    if(arrayObj != "clearList"){
+
+        if(arrayObj.length === undefined){
+    
+            let {id, name, lastname} = arrayObj;
+    
+            usersContent += `
+                <li class="bg-dark text-white list-group-item">ID: ${id} <br> NAME: ${name} <br> LASTNAME: ${lastname}</li> 
+            `;
+    
+        }else{
+    
+            for(let i=0; i < arrayObj.length; i++){
+    
+                let user = arrayObj[i];
+                let {id, name, lastname} = user;
+        
+                usersContent += `
+                    <li class="bg-dark text-white list-group-item">ID: ${id} <br> NAME: ${name} <br> LASTNAME: ${lastname}</li>
+                `;
+        
+            }
+    
+        }
+
+    }else if(arrayObj === "clearList"){
+        searchInput.value = "";
+    }
+
+    resultsList.innerHTML = usersContent;
+};
+
 
 const  getJSONData = async (url) => {
     let result = {};
@@ -24,11 +111,7 @@ const  getJSONData = async (url) => {
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const resultObj = await getJSONData(mokapiUrl)
-    if (resultObj.status === "ok"){
-        users = resultObj;
-        console.log(users);  
-    }
+   
 
 });
 
